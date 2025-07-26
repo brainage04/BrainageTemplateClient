fe86bc7b5d284c8d8f15f540fb7e6a088375e146
brainage04 <thomasmcmah21@gmail.com>
2025-07-27 00:34:05 +1000

    added template command

---
+++ src/main/java/io/github/brainage04/brainagetemplateclient/command/TemplateCommand.java
@@ -0,0 +1,25 @@
+package io.github.brainage04.brainagetemplateclient.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.text.Text;
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+
+public class TemplateCommand {
+    public static int execute(FabricClientCommandSource source) {
+        source.sendFeedback(Text.literal("This is a test command."));
+
+        return 1;
+    }
+
+    public static void initialize(CommandDispatcher<FabricClientCommandSource> dispatcher) {
+        dispatcher.register(literal("template")
+                .executes(context ->
+                        execute(
+                                context.getSource()
+                        )
+                )
+        );
+    }
+}
--- src/main/java/io/github/brainage04/brainagetemplateclient/command/core/ModCommands.java
+++ src/main/java/io/github/brainage04/brainagetemplateclient/command/core/ModCommands.java
@@ -1,7 +1,12 @@
 package io.github.brainage04.brainagetemplateclient.command.core;
 
+import io.github.brainage04.brainagetemplateclient.command.TemplateCommand;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+
 public class ModCommands {
     public static void initialize() {
-
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            TemplateCommand.initialize(dispatcher);
+        });
     }
 }
