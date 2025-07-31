776466eca8a801742da542ccda8c2a8ce7c5deab
brainage04 <thomasmcmah21@gmail.com>
2025-07-31 13:18:20 +1000

    added example screen + refactored TemplateCommand to ExampleCommand

--- src/main/java/com/example/command/ExampleCommand.java
+++ src/main/java/com/example/command/ExampleCommand.java
@@ -6,15 +6,15 @@ import net.minecraft.text.Text;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
-public class TemplateCommand {
+public class ExampleCommand {
     public static int execute(FabricClientCommandSource source) {
-        source.sendFeedback(Text.literal("This is a test command."));
+        source.sendFeedback(Text.literal("This is an example command."));
 
         return 1;
     }
 
     public static void initialize(CommandDispatcher<FabricClientCommandSource> dispatcher) {
-        dispatcher.register(literal("template")
+        dispatcher.register(literal("example")
                 .executes(context ->
                         execute(
                                 context.getSource()
--- src/main/java/com/example/command/core/ModCommands.java
+++ src/main/java/com/example/command/core/ModCommands.java
@@ -1,12 +1,15 @@
 package com.example.command.core;
 
-import com.example.command.TemplateCommand;
+import com.example.command.ExampleCommand;
+import com.example.command.screen.OpenExampleScreenCommand;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
 public class ModCommands {
     public static void initialize() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-            TemplateCommand.initialize(dispatcher);
+            OpenExampleScreenCommand.initialize(dispatcher);
+
+            ExampleCommand.initialize(dispatcher);
         });
     }
 }
